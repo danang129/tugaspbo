285a58e5cb38dced3688af4395dad702786c2870
danang129 <ahmaddanangsetiawan4@gmail.com>
2024-05-13 11:51:28 +0700

    commit login & register

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 });
 
 Route::get('/login',[UserController::class, 'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
